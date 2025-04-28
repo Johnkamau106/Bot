c96866852177b8a9694083f30b5383d22f1705bd
johnkamau106 <john.kamau7@student.moringaschool.com>
2025-04-28 06:39:53 +0300

    restarted the project

--- src/Components/BotCollection.jsx
+++ src/Components/BotCollection.jsx
@@ -4,17 +4,7 @@ import BotCard from './BotCard';
 
 function BotCollection({ bots, addBotToArmy }) {
   return (
-    <div>
-      <h2>Available Bots</h2>
-      <div className="bot-cards">
-        {bots.map((bot) => (
-          <div key={bot.id}>
-            <BotCard bot={bot} />
-            <button onClick={() => addBotToArmy(bot)}>Add to Army</button>
-          </div>
-        ))}
-      </div>
-    </div>
+    
   );
 }
 
