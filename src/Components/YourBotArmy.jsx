83e3f3a8fa84773a691262f74a586653af0175ea
johnkamau106 <john.kamau7@student.moringaschool.com>
2025-04-28 06:45:39 +0300

    finished on yourbotarmy

--- src/Components/YourBotArmy.jsx
+++ src/Components/YourBotArmy.jsx
@@ -4,7 +4,22 @@ import BotCard from './BotCard';
 
 function YourBotArmy({ yourArmy, removeBotFromArmy, dischargeBot }) {
   return (
-   
+    <div>
+      <h2>Your Bot Army</h2>
+      <div className="bot-cards">
+        {yourArmy.map((bot) => (
+          <div key={bot.id} className="bot-wrapper">
+            <BotCard bot={bot} onClick={removeBotFromArmy} />
+            <button
+              className="discharge-button"
+              onClick={() => dischargeBot(bot.id)}
+            >
+              X
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
