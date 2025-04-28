7253dbcfbdb22bbc033bf4fd1781dd76de1a5595
johnkamau106 <john.kamau7@student.moringaschool.com>
2025-04-28 06:42:55 +0300

    started the appjsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// App.js
+import React, { useState, useEffect } from 'react';
+import BotCollection from './Components/BotCollection';
+import YourBotArmy from './Components/YourBotArmy';
+import './App.css';
+
+
+  return (
+    
+  );
+}
+
+export default App;
